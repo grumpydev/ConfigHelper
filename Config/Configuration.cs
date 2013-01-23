508241377cd5213e42f2d9fb0eec22f50187dd33
Steven Robbins <ste.robbins@gmail.com>
2013-01-23 10:27:59 +0000

    Initial commit

---
+++ Config/Configuration.cs
@@ -0,0 +1,44 @@
+﻿namespace Config
+{
+    /// <summary>
+    /// Sample configuration class
+    /// Contains static and instance properties of various types
+    /// </summary>
+    public class Configuration
+    {
+        public string InstanceStringValue { get; set; }
+
+        public static string StaticStringValue { get; set; }
+
+        public int InstanceIntValue { get; set; }
+
+        public static int StaticIntValue { get; set; }
+
+        public bool InstanceBoolValue { get; set; }
+
+        public static bool StaticBoolValue { get; set; }
+
+        public string CommentedOut { get; set; }
+
+        /// <summary>
+        /// Set some defaults for the static props
+        /// </summary>
+        static Configuration()
+        {
+            StaticStringValue = "Not set";
+            StaticIntValue = -1;
+            StaticBoolValue = false;
+        }
+
+        /// <summary>
+        /// Set some defaults for the instance props
+        /// </summary>
+        public Configuration()
+        {
+            InstanceStringValue = "Not set";
+            InstanceIntValue = -1;
+            InstanceBoolValue = false;
+            CommentedOut = "Not set";
+        }
+    }
+}
---
+++ Config/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Config
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new Configuration();
+
+            Console.WriteLine("Config before loading:\n{0}", DumpConfig(config));
+            Console.WriteLine();
+            ConfigLoader.Load(@"Deploy.config", config);
+
+            Console.WriteLine("Config after loading:\n{0}", DumpConfig(config));
+        }
+
+        private static string DumpConfig(Configuration config)
+        {
+            return string.Format("\tInstanceStringValue: {0}\n\tInstanceIntValue: {1}\n\tInstanceBoolValue: {2}\n\tCommentedOut: {3}\n\tStaticStringValue: {4}\n\tStaticIntValue: {5}\n\tStaticBoolValue: {6}", 
+                                    config.InstanceStringValue, 
+                                    config.InstanceIntValue, 
+                                    config.InstanceBoolValue, 
+                                    config.CommentedOut,
+                                    Configuration.StaticStringValue,
+                                    Configuration.StaticIntValue,
+                                    Configuration.StaticBoolValue);
+        }
+    }
+}
